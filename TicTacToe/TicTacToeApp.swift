54c91b4e7367be83eab22202d8516dcfd23f5b77
Brandon Johns <brandonjohns914@yahoo.com>
2022-10-27 15:29:08 -0700

    Initial Commit

---
+++ TicTacToe/TicTacToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToeApp.swift
+//  TicTacToe
+//
+//  Created by Brandon Johns on 10/27/22.
+//
+
+import SwiftUI
+
+@main
+struct TicTacToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
